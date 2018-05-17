75a9e34aa7c845dc41c520c2e16fd988cfc01a40
James Luterek <james.luterek@gmail.com>
2018-05-17 15:23:24 -0400

    Exercise1 - RequestBuilder

--- exercise1.js
+++ exercise1.js
@@ -1,9 +1,24 @@
 const client = require('./handson/client.js');
 const projectKey = 'sdk-training-project';
+const sdkRequestBuilder = require('@commercetools/api-request-builder');
 
 client.getClient().then((client) => {
   console.log(client);
 
+  const requestBuilder = sdkRequestBuilder.createRequestBuilder({ projectKey });
+  const projectUri = requestBuilder.project.build();
+  const projectRequest = {
+    uri: projectUri,
+    method: 'GET'
+  };
+
+  client.execute(projectRequest).then(result => {
+    console.log(result);
+  });
+
+  //API Calls can be made without request builder.
+  //Specify the uri manually
+  /*
   const request = {
     uri: `/${projectKey}`,
     method: 'GET'
@@ -12,5 +27,6 @@ client.getClient().then((client) => {
   client.execute(request)
   .then(result => console.log(result))
   .catch(error => console.log(error));
+  */
 
 });
