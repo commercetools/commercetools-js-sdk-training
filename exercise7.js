98c9cd20704504d01fcac621c1570020eca85f75
Tori Hall <tori.hall@commercetools.com>
2020-01-07 16:33:19 +0100

    Delete old exercise

--- exercise7.js
+++
@@ -1,8 +0,0 @@
-const { getCart } = require('./handson/cart.js');
-const log = require('./logger.js').log;
-
-// TODO 7.1: Implement the getCart function in ./handson/cart.js
-
-getCart()
-  .then(log)
-  .catch(log);
