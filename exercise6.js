a993a81424f90ed8c26d78c817c5c75df95fcfe6
Tori Hall <tori.hall@commercetools.com>
2020-01-07 16:33:10 +0100

    Delete old exercise

--- exercise6.js
+++
@@ -1,8 +0,0 @@
-const { createCart } = require('./handson/cart.js');
-const log = require('./logger.js').log;
-
-// TODO 6.1: Complete the createCart function in ./handson/cart.js
-
-createCart("USD", "US", "en")
-  .then(log)
-  .catch(log);
