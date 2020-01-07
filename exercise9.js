8a5f3695dcd16ee68deab86d930c26ade54001f7
Tori Hall <tori.hall@commercetools.com>
2020-01-07 16:33:40 +0100

    Delete old exercise

--- exercise9.js
+++
@@ -1,8 +0,0 @@
-const { captureOrder } = require('./handson/cart.js');
-const log = require('./logger.js').log;
-
-// TODO 9.1: Complete the captureOrder function in ./handson/cart.js
-
-captureOrder()
-  .then(log)
-  .catch(log);
