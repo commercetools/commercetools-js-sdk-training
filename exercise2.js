ab54866d9868bde10196e94f963b26cf5326cb38
Fady Fawzy <fady.fawzy@commercetools.de>
2020-01-08 11:01:11 +0100

    Latest Updates by Michael and removing old files

--- exercise2.js
+++
@@ -1,9 +0,0 @@
-const { getProductTypes } = require('./handson/product.js');
-
-// Complete getProductTypes function in ./handson/product.js
-
-getProductTypes().then(ptList => {
-  console.log(JSON.stringify(ptList, null, 4));
-}).catch(error => {
-  console.log(JSON.stringify(error, null, 4));
-});
--- exercise3.js
+++
@@ -1,9 +0,0 @@
-const { getTaxCategories } = require('./handson/taxCategory.js');
-
-// Complete getTaxCategories function in ./handson/taxCategory.js
-
-getTaxCategories().then(taxList => {
-  console.log(JSON.stringify(taxList, null, 4));
-}).catch(error => {
-  console.log(JSON.stringify(error, null, 4));
-});
