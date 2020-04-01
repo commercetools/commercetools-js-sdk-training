a1edfe4f8061347aaffc17f794f9d7ffa11190a6
Tori Hall <tori.hall@commercetools.com>
2020-04-01 14:18:20 -0400

    fixed typo

--- exercise11_UPDATE.js
+++ exercise11_UPDATE.js
@@ -1,7 +1,7 @@
 const { getClient, projectKey } = require('./handson/client.js');
 const { getProductByKey, 
         getCategoryByKey, 
-        assignProductToCategoryPROMISSES,
+        assignProductToCategoryPROMISES,
         assignProductToCategoryAwait } = require('./handson/product.js');
 const log = require('./logger.js').log;
 const { createRequestBuilder } = require('@commercetools/api-request-builder');
@@ -17,8 +17,8 @@ getCategoryByKey('wine-category').then(log).catch(log);
 // Day 2
 // TODO Assign product to category and publish
 
-// VERSION 1: Using Promisses
-assignProductToCategoryPROMISSES('green-bordeaux', 'wine-category').then(log).catch(log);
+// VERSION 1: Using Promises
+assignProductToCategoryPROMISES('green-bordeaux', 'wine-category').then(log).catch(log);
 
 // VERSION 2: Using Await
 assignProductToCategoryAwait('green-bordeaux', 'wine-category').then(log).catch(log);
